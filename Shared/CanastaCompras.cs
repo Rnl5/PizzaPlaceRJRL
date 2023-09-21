39c14279def5d49d2e7f1cd34e65605400816db9
Rnl5 <143105578+Rnl5@users.noreply.github.com>
2023-09-21 17:04:45 -0400

    Se agrega lo que el usuario vera en su canasta de compras y se agrega el metodo eliminar en la clase CanastaCompras

--- Shared/CanastaCompras.cs
+++ Shared/CanastaCompras.cs
@@ -9,4 +9,6 @@ public class CanastaCompras
     public bool Pagado{get; set;}
 
     public void Agregar(int pizzaId) => Ordenes.Add(pizzaId);
+
+    public void Eliminar(int pos) => Ordenes.RemoveAt(pos);
 }
